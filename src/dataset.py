f75c5d492dc579c2e641f2afdfda2f45f7fbfbe3
Mika <mikards.lini@gmail.com>
2026-09-08 19:26:52 +0300

    Add EDA and knee image notebooks, update dataset loader

---
+++ src/dataset.py
@@ -0,0 +1,11 @@
+import os
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+print("hello")
+print("this is a test")
+
