5ed6842d5139f1556e836aa8ca76ed391e765b35
Mika <mikards.lini@gmail.com>
2026-09-24 09:35:11 +0300

    minor

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -6,7 +6,7 @@ class CNN(nn.Module):
     def __init__(self, num_classes=5):
         super().__init__()
 
-        # Convolutional feature extractor
+        # Convolutional layers
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
